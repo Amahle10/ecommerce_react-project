99a759733d93ae0286376079fd6d20674a74094c
amahle10 <acepten.b@gmail.com>
2026-05-15 15:14:12 +0200

    fix dotenv

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,4 +1,6 @@
 // backend/routes/orderRoutes.js
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import Order from "../models/Order.js";
--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import Order from "../models/Order.js";
